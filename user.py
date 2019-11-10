c3d8f387116ff4cb4ee8371665eb35124b77668e
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-10 22:15:56 +0300

    add instance variables to class user.py file

--- user.py
+++ user.py
@@ -18,6 +18,7 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        
         self.phone_number = phone_number
         self.password = password
 
