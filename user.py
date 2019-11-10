40f0785f2a6606ff0545e97dfad276f6b2397672
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-10 22:16:52 +0300

    fixed indentation error in class user.py file

--- user.py
+++ user.py
@@ -18,7 +18,6 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        
         self.phone_number = phone_number
         self.password = password
 
