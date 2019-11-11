26f3654082863163737cf11e758be547d71bda4d
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-11 08:14:36 +0300

    add a function that creates a new user in password_locker.py

--- password_locker.py
+++ password_locker.py
@@ -13,6 +13,15 @@ def create_user(first_name, last_name, email, phone_number, password):
     return new_user
 
 
+
+# def create_user(first_name, last_name, email, phone_number, password):
+#     '''
+#     Function to create a new user account
+#     '''
+#     new_user = User(first_name, last_name, email, phone_number, password)
+#     return new_user
+
+
 def create_credential(user_name, site_name, account_name, password):
     '''
     Function to create a new user account
